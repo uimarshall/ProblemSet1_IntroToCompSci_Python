b7df1d1e736417892d811ad0cbf05230886b2710
uimarshall <uimarshall@gmail.com>
2018-09-04 16:31:44 +0100

    added ps0.py file

---
+++ ps0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  6 15:55:53 2018
+
+@author: USER
+"""
+"""Team Members: Chizokam and Emeka"""
+import math
+x = int(input('Enter a number x: '))
+y = int(input('Enter a number y: '))
+z = x**y
+log_num = math.log(x,2)
+print('x ** y = %d' %(z))
+print('Log(%d) = %d' %(x, log_num))
